21e108da4f33b3aec423ace4aeb0fb00412f0039
dawsonhammdev <hammsdesigns@gmail.com>
2020-04-10 10:32:38 -0500

    I have installed axios and received the data I need from the USA soccer team

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      teams: []
+    }
+  };
+
+  componentDidMount() {
+    axios
+    .get('http://localhost:5000/api/players')
+    .then(res => {
+     this.setState({
+        teams: res.data
+      })
+      console.log("API data:", res.data)
+    })
+  }
+
+
+
+
+
+
+
+
+  render() {
+    return (
+      <div>
+        <h1>World Cup Teams Rankings</h1>
+      </div>
+    )
+  }
+
 }
 
 export default App;
