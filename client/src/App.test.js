987366f98e91c09a9401e107250a4c01400fb2d0
dawsonhammdev <hammsdesigns@gmail.com>
2020-04-10 11:55:58 -0500

    I have added my tests and styled the player cards

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('Is the Heading Visible On the Page?', () => {
+  //Arrange- Set up testing enviornment
+  const { getByText } = render(<App/>)
+
+  //No action
+
+  //Assert
+  const linkElement = getByText(/World Cup Teams Rankings/i);
+
 });
+
+test('Does Dark Mode Button Work?', () => {
+
+  const { getByText } = render(<App/>)
+
+  fireEvent.click(getByText(/dark mode/i));
+
+});
+
+test('Is The NavBar Visible?', () => {
+  const{ getByText } = render(<App/>)
+  const linkElement = getByText(/Dark Mode/i)
+})
+
+
--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function PlayerCard(props) {
     return (
-        <div>
+        <div className="player-card">
             {props.player_data.map(player => (
                 <div className="card-container" key={player.id}>
                     <h2>{player.name}</h2>
