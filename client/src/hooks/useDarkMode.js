0bc91059fd11e9c6b24eb18af20b2946320de5c0
dawsonhammdev <hammsdesigns@gmail.com>
2020-04-10 11:18:54 -0500

    I have added the darkmode hook to my nav bar with a button that switches to dark mode

--- client/src/hooks/useDarkMode.js
+++ client/src/hooks/useDarkMode.js
@@ -1,12 +1,9 @@
   
-// step 7: create a useDarkMode costum hook to change your site to dark
-// use your local storage hook in here that way when a user reloads the site,
-// it will keep their dark mode/light mode status
 import { useLocalStorage } from "./useLocalStorage";
 import { useEffect } from "react";
 
 export const useDarkMode = () => {
-  const [values, setValues] = useLocalStorage("dark-mode", false); // if you switch this to true, it will be dark mode when you reload
+  const [values, setValues] = useLocalStorage("dark-mode", false); 
   const body = document.querySelector("body");
 
   useEffect(() => {
